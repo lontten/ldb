c24ecf8a2cf72447daf3e109059f1ca5fbd37839
lontten <lontten@163.com>
2025-09-19 11:08:33 +0800

    Update insert_test.go

--- benchmark/insert_test.go
+++ benchmark/insert_test.go
@@ -1,24 +1,23 @@
 package benchmark
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/lontten/ldb/v2"
 )
 
 func BenchmarkInsert_mysql(b *testing.B) {
-	// 定义测试对象
-	u := User{
-		Id:    0,
-		Name:  "tom",
-		Email: "xx@xx.com",
-	}
-
-	// 重置计时器（排除初始化时间）
 	b.ResetTimer()
 
 	// 执行b.N次（基准测试核心循环）
 	for i := 0; i < b.N; i++ {
+		u := User{
+			Id:    0,
+			Name:  "tom",
+			Email: fmt.Sprintf("xx%d@xx.com", i),
+		}
+
 		_, err := ldb.Insert(DB, u, ldb.E().ShowSql())
 		if err != nil {
 			b.Errorf("insert failed: %v", err)
