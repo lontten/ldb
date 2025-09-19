4534f150a27b306453e892acc14d181f6a05cc65
lontten <lontten@163.com>
2025-09-19 16:37:24 +0800

    1

--- benchmark/insert_test.go
+++ benchmark/insert_test.go
@@ -7,7 +7,38 @@ import (
 	"github.com/lontten/ldb/v2"
 )
 
-func BenchmarkInsert(b *testing.B) {
+func BenchmarkInsert_ldb(b *testing.B) {
+	_, err := ldb.Exec(DB, "DELETE FROM users WHERE 1=1")
+	if err != nil {
+		b.Fatalf("初始化清理数据失败: %v", err)
+	}
+
+	b.Cleanup(func() {
+		_, err := ldb.Exec(DB, "DELETE FROM users WHERE 1=1")
+		if err != nil {
+			b.Logf("测试后清理数据失败: %v", err)
+		}
+	})
+
+	b.ResetTimer()
+
+	// 执行b.N次（基准测试核心循环）
+	for i := 0; i < b.N; i++ {
+		u := User{
+			Id:    0,
+			Name:  "tom",
+			Email: fmt.Sprintf("xx%d@xx.com", i),
+		}
+
+		_, err := ldb.Insert(DB, u)
+		if err != nil {
+			b.Errorf("insert failed: %v", err)
+		}
+	}
+
+}
+
+func BenchmarkInsert_gorm(b *testing.B) {
 	_, err := ldb.Exec(DB, "DELETE FROM users WHERE 1=1")
 	if err != nil {
 		b.Fatalf("初始化清理数据失败: %v", err)
--- benchmark/select_test.go
+++ benchmark/select_test.go
@@ -7,7 +7,40 @@ import (
 	"github.com/lontten/ldb/v2"
 )
 
-func BenchmarkSelect(b *testing.B) {
+func BenchmarkSelect_ldb(b *testing.B) {
+	for i := 0; i < 100; i++ {
+		u := User{
+			Id:    0,
+			Name:  "tom",
+			Email: fmt.Sprintf("xx%d@xx.com", i),
+		}
+
+		_, err := ldb.Insert(DB, u)
+		if err != nil {
+			b.Errorf("insert failed: %v", err)
+		}
+	}
+
+	b.Cleanup(func() {
+		_, err := ldb.Exec(DB, "DELETE FROM users WHERE 1=1")
+		if err != nil {
+			b.Logf("测试后清理数据失败: %v", err)
+		}
+	})
+
+	b.ResetTimer()
+
+	// 执行b.N次（基准测试核心循环）
+	for i := 0; i < b.N; i++ {
+		_, err := ldb.List[User](DB, ldb.W().Eq("1", 1))
+		if err != nil {
+			b.Errorf("insert failed: %v", err)
+		}
+	}
+
+}
+
+func BenchmarkSelect_gorm(b *testing.B) {
 	for i := 0; i < 100; i++ {
 		u := User{
 			Id:    0,
