318deacd83f8fcbf9147dbe9f71d4d91ea2f4eda
lontten <lontten@163.com>
2025-11-14 10:37:09 +0800

    恢复Transaction

--- transaction.go
+++ transaction.go
@@ -5,46 +5,7 @@ import (
 	"log"
 )
 
-func Transaction[T any](db Engine, fn func(tx Engine) T) (res T, err error) {
-	tx, err := db.Begin()
-	if err != nil {
-		return res, err
-	}
-
-	defer func() {
-		r := recover()
-		if r != nil {
-			rbErr := tx.Rollback()
-			if rbErr != nil {
-				log.Printf("rollback failed: %v", rbErr)
-			}
-
-			if e, ok := r.(error); ok {
-				err = e
-			} else {
-				err = fmt.Errorf("panic: %v", r)
-			}
-			return
-		}
-	}()
-
-	res = fn(tx)
-	if err != nil {
-		rbErr := tx.Rollback()
-		if rbErr != nil {
-			log.Printf("rollback failed: %v", rbErr)
-		}
-		return res, err
-	}
-
-	if err = tx.Commit(); err != nil {
-		return res, fmt.Errorf("commit failed: %v", err)
-	}
-
-	return res, nil
-}
-
-func TransactionErr[T any](db Engine, fn func(tx Engine) (T, error)) (res T, err error) {
+func Transaction[T any](db Engine, fn func(tx Engine) (T, error)) (res T, err error) {
 	tx, err := db.Begin()
 	if err != nil {
 		return res, err
