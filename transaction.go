779e2b89fb1f53d6c14a09d6ec6f7ef4845e751a
lontten <lontten@163.com>
2025-11-18 11:42:55 +0800

    Transaction添加返回数据逻辑

--- transaction.go
+++ transaction.go
@@ -5,6 +5,16 @@ import (
 	"log"
 )
 
+// 回滚结构体
+type RollbackWithResult[T any] struct {
+	Result T
+}
+
+// 辅助函数，让业务代码更清晰
+func Rollback[T any](result T) {
+	panic(RollbackWithResult[T]{Result: result})
+}
+
 func Transaction[T any](db Engine, fn func(tx Engine) (T, error)) (res T, err error) {
 	tx, err := db.Begin()
 	if err != nil {
@@ -19,10 +29,16 @@ func Transaction[T any](db Engine, fn func(tx Engine) (T, error)) (res T, err er
 				log.Printf("rollback failed: %v", rbErr)
 			}
 
-			if e, ok := r.(error); ok {
-				err = e
-			} else {
-				err = fmt.Errorf("panic: %v", r)
+			// 检查是否是自定义的回滚类型
+			switch v := r.(type) {
+			case RollbackWithResult[T]:
+				// 如果是自定义回滚，返回指定的结果，没有错误
+				res = v.Result
+				err = nil
+			case error:
+				err = v
+			default:
+				err = fmt.Errorf("%v", err)
 			}
 			return
 		}
